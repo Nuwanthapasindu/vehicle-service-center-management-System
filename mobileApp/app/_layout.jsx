be3620a1b87896f4dd77ce2e9f492a9cea9714f7
Nuwanthapasindu <hnpkdias@gmail.com>
2026-02-23 10:03:51 +0530

    feat: Implement initial onboarding screen with a carousel, assets, and color constants, and set up the root layout.

---
+++ mobileApp/app/_layout.jsx
@@ -0,0 +1,6 @@
+import { View, Text } from 'react-native'
+import { Slot } from 'expo-router'
+
+export default function RootLayout() {
+  return <Slot />
+}
---
+++ mobileApp/constants/colors.js
@@ -0,0 +1,11 @@
+const colors = Object.freeze({
+    PRIMARY: "#8EDB00",
+    BACKGROUND_COLOR:"#F8F9FA",
+    LIGHT:"#FFFFFF",
+    DARK:"#1A1D23",
+    SECONDARY: "#64748B",
+    BORDER_COLOR:"#E2E8F0",
+
+})
+
+export default colors;
