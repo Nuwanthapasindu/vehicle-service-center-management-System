b15807f135f6aded13ace8a58b6b77dd3ae69bf5
Nuwantha <hnpkdias@gmail.com>
2026-02-27 17:47:56 +0530

    feat: add temperory logout utton

--- mobileApp/app/(protected)/Home.jsx
+++ mobileApp/app/(protected)/Home.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import useAuthentication from '../../hooks/useAuth'
 
 export default function Home() {
+  const {logout} = useAuthentication()
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text>Home</Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
