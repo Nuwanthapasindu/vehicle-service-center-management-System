51bfa68125d002de67d3603833d6cab87b9186ad
Nuwantha <hnpkdias@gmail.com>
2026-02-27 11:01:56 +0530

    refactor: Remove Redux Provider and Toast component from the root layout.

--- mobileApp/app/_layout.jsx
+++ mobileApp/app/_layout.jsx
@@ -3,8 +3,6 @@ import { Slot, useRouter } from "expo-router";
 import useAsyncStorage from "../hooks/useAsyncStorage";
 import storageKeys from "../constants/storageKeys";
 
-import { Provider } from "react-redux";
-import { store } from "../store/store";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
@@ -21,10 +19,5 @@ export default function RootLayout() {
     checkOnboardingStatus();
   }, [router]);
 
-  return (
-    <Provider store={store}>
-      <Slot />
-      <Toast />
-    </Provider>
-  );
+  return <Slot />;
 }
