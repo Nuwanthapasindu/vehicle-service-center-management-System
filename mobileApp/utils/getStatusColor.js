2c0063d68ac18269e44395907d0562530ee4befc
Thisal Dilmith <93121062+thisal-d@users.noreply.github.com>
2026-04-17 11:36:29 +0530

    fix(enums): use enums instead of hardcoded values

--- mobileApp/utils/getStatusColor.js
+++ mobileApp/utils/getStatusColor.js
@@ -1,10 +1,17 @@
+import enums from "../constants/enums";
+
 const getStatusColor = (status) => {
   switch (status) {
-    case 'PENDING': return '#F59E0B'; // Orange
-    case 'START': return '#3B82F6'; // Blue
-    case 'FINISH': return '#10B981'; // Green
-    default: return '#F59E0B';
+    case enums.JOBCARD_STATUS.PENDING:
+      return "#F59E0B"; // Orange
+    case enums.JOBCARD_STATUS.START:
+      return "#3B82F6"; // Blue
+    case enums.JOBCARD_STATUS.FINISH:
+      return "#10B981"; // Green
+    default:
+      return "#F59E0B";
   }
 };
 
 export default getStatusColor;
+
