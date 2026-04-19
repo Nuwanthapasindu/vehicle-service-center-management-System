9c8c66f7c59cde25959fe41b838f297ecd628907
Nuwantha <hnpkdias@gmail.com>
2026-04-20 01:41:40 +0530

    feat: add accessControl middleware for role-based authorization

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -45,4 +45,32 @@ const authTokenMiddleware = async (request, response, next) => {
   }
 };
 
-module.exports = { authTokenMiddleware };
+/**
+ * Access control middleware
+ * 
+ * @param {string[]} roles - list of allowed roles
+ * 
+ * @returns {(req: Request, res: Response, next: NextFunction) => void} - middleware function
+ * 
+ * @throws {AppError} - if roles is not an array or if roles is empty
+ * @throws {AppError} - if user role is not in roles
+ */
+const accessControl = (roles) => {
+  return (req, res, next) => {
+    if (Array.isArray(roles) && roles.length > 0) {
+      const user = req.user;
+      if (roles.includes(user.role)) {
+        next();
+      } else {
+        next(new AppError(`Access denied for ${user.role} users`, 403));
+        return;
+      }
+    } else {
+      next(new AppError("Invalid role(s) specified", 400));
+    }
+  };
+}
+
+
+module.exports = { authTokenMiddleware, accessControl };
+
