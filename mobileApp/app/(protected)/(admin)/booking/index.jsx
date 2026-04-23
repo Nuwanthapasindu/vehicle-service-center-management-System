393b69adcfb6c7cd148db17c5882975938555c72
Thisal Dilmith <93121062+thisal-d@users.noreply.github.com>
2026-04-23 10:40:39 +0530

    fix(booking): extend mobile app booking viewing date to 30

--- mobileApp/app/(protected)/(admin)/booking/index.jsx
+++ mobileApp/app/(protected)/(admin)/booking/index.jsx
@@ -37,7 +37,7 @@ export default function Bookings() {
 
 
   // Generate next viewing days
-  const DAYS_COUNT = 30;
+  const DAYS_COUNT = 31;
   const dates = useMemo(() => generateNextDays(DAYS_COUNT), []);
 
 
