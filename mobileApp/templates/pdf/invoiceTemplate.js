634b1f8e0f643fe2b86ef685e4a65c5ff91ab55e
Nuwantha <hnpkdias@gmail.com>
2026-07-20 11:55:08 +0530

    fix: update logo URL in invoice template

--- mobileApp/templates/pdf/invoiceTemplate.js
+++ mobileApp/templates/pdf/invoiceTemplate.js
@@ -114,7 +114,7 @@ export const getInvoiceTemplate = (invoice = {}) => {
   <body>
     <div class="header">
       <div class="header-left">
-        <img src="${logoUri}" class="logo" alt="Logo" />
+        <img src="https://shinedepot.lk/logo.jpeg" class="logo" alt="Logo" />
         <div class="company-details">
           <div class="company-name">SHINE DEPOT</div>
           <div class="company-info">108 Old Kottawa Rd, Nugegoda</div>
